074b16308ed25f770cf40a9b894c613c23754f75
Alemol <33874617+alemolamg@users.noreply.github.com>
2020-11-02 18:35:14 +0100

    ADD tenemos más preguntas, en total 6

--- src/juegoTrivial/Juego.java
+++ src/juegoTrivial/Juego.java
@@ -1,5 +1,9 @@
 package juegoTrivial;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 import juegoTrivial.Pregunta;
 
 public class Juego {
@@ -32,32 +36,69 @@ public class Juego {
 	public static void main(String[] args) {
 		textoBienvenida();
 		
+		List<Pregunta> listaPreguntas = new ArrayList<Pregunta>();
+		
+		crearPreguntas(listaPreguntas);
+		
+		System.out.println("\n\n  Comenzamos con la ronda de preguntas:  ");
+		
+		for (Pregunta iterator : listaPreguntas ) {
+			
+		}
+		
 		
 	}
 	
 	
-	public static void preguntas(String vectorPreguntas []) {
-		Pregunta p1 = new Pregunta ("¿ Quién fue el primer presidente de la democracia española tras el franquismo?");
-		Pregunta p2 = new Pregunta("Para qué religión es especialmente importante el rey Haile Selassie I");
-		Pregunta p3 = new Pregunta("¿Cuál es el nombre de la capital de Albania?");
+	public static void crearPreguntas(List<Pregunta> todasLasPreguntas) {
+				
+		List<Respuesta> listaR1 = new ArrayList<Respuesta>();
+		listaR1.add(new Respuesta ("José Calvo Sotero", false));
+		listaR1.add(new Respuesta ("Manuel Azaña", false));
+		listaR1.add(new Respuesta ("Felipe González", false));
+		listaR1.add(new Respuesta ("Adolfo Suarez", true));
 		
 		
-		Respuesta r11 = new Respuesta ("José Calvo Sotero", false);
-		Respuesta r12 = new Respuesta ("Manuel Azaña", false);
-		Respuesta r13 = new Respuesta ("Felipe González", false);
-		Respuesta r14 = new Respuesta ("Adolfo Suarez", true);
-		
-		Respuesta r21 = new Respuesta ("Rastafari", true);
-		Respuesta r22 = new Respuesta ("Jedismo", false);
-		Respuesta r23 = new Respuesta ("Nuwaubianismo", false);
-		Respuesta r24 = new Respuesta ("hinduismo", false);  
+		List<Respuesta> listaR2 = new ArrayList<Respuesta>();
+		listaR2.add(new Respuesta ("Rastafari", true));
+		listaR2.add(new Respuesta ("Jedismo", false));
+		listaR2.add(new Respuesta ("Nuwaubianismo", false));
+		listaR2.add(new Respuesta ("hinduismo", false));  
 
-		Respuesta r31 = new Respuesta ("Gitega", false);
-		Respuesta r32 = new Respuesta ("Tirana", true);
-		Respuesta r33 = new Respuesta ("Moroni", false);
-		Respuesta r34 = new Respuesta ("Lusaka", false);
+		List<Respuesta> listaR3 = new ArrayList<Respuesta>();
+		listaR3.add(new Respuesta ("Gitega", false));
+		listaR3.add(new Respuesta ("Tirana", true));
+		listaR3.add(new Respuesta ("Moroni", false));
+		listaR3.add(new Respuesta ("Lusaka", false));
+		
+		List<Respuesta> listaR4 = new ArrayList<Respuesta>();
+		listaR4.add(new Respuesta("2000", false));
+		listaR4.add(new Respuesta("1999", true));
+		listaR4.add( new Respuesta("2001", false));
+		listaR4.add(new Respuesta("1998", false));
+		
+		List<Respuesta> listaR5 = new ArrayList<Respuesta>();
+		listaR5.add(new Respuesta("Pícaro", false));
+		listaR5.add(new Respuesta("Rocinante", true));
+		listaR5.add(new Respuesta("Dorado", false));
+		listaR5.add(new Respuesta("Babieca",false));
 		
 		
+		List<Respuesta> listaR6 = new ArrayList<Respuesta>();
+		listaR6.add(new Respuesta("Tango", false));
+		listaR6.add(new Respuesta("Frevo", false));
+		listaR6.add(new Respuesta("Capoeira", true));
+		listaR6.add(new Respuesta("Kathakali", false));
+		
+		
+		todasLasPreguntas.add(new Pregunta ("¿ Quién fue el primer presidente de la "
+				+ "democracia española tras el franquismo?", listaR1));
+		todasLasPreguntas.add(new Pregunta("Para qué religión es especialmente importante el rey Haile Selassie I",listaR2));
+		todasLasPreguntas.add(new Pregunta("¿Cuál es el nombre de la capital de Albania?",listaR3));
+		todasLasPreguntas.add(new Pregunta("¿Cuándo se estrenó la película The Matrix?",listaR4));
+		todasLasPreguntas.add(new Pregunta("¿Cómo se llamaba el caballo que pertenecía a "
+				+ "Don Quijote de La Mancha?", listaR5));
+		todasLasPreguntas.add(new Pregunta("¿Qué baile tiene su origen en un arte marcial?", listaR6));
 		
 		
 	}
--- src/juegoTrivial/Pregunta.java
+++ src/juegoTrivial/Pregunta.java
@@ -8,12 +8,10 @@ public class Pregunta {
 	private String textoPregunta;
 	public List<Respuesta> listaRespuestas = new ArrayList<Respuesta>();
 	
-	public Pregunta(String texto) {
+	public Pregunta(String texto, List<Respuesta> respuestasPregunta) {
 		textoPregunta = texto;
-		
+		listaRespuestas = respuestasPregunta;		
 	}
-	
-	
 	
 	
 
